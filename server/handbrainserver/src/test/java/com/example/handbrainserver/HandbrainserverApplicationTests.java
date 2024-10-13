ec912680c4227607f9f2f7a220bf32370ff634d1
noir-01 <smu08054@gmail.com>
2024-10-13 16:29:08 +0900

    init spring server

---
+++ server/handbrainserver/src/test/java/com/example/handbrainserver/HandbrainserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handbrainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandbrainserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
