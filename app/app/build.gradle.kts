2e5e4d052efc3af29838994c1a369fcaac4533ab
blanc <blanc@DESKTOP-23KS9M5>
2024-10-15 00:00:04 +0900

    Feat: 손 동작 인식 기능 추가

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -59,6 +59,31 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+
+    // Jetpack Compose 라이브러리 추가
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    // CameraX libraries
+    //implementation("androidx.camera:camera-core:1.1.0")
+    implementation("androidx.camera:camera-camera2:1.2.0")
+    implementation("androidx.camera:camera-lifecycle:1.2.0")
+    implementation("androidx.camera:camera-view:1.2.0")
+
+    //mediapipe
+    implementation("com.google.mediapipe:tasks-vision:0.10.14")
+
+    //KNN
+    implementation("com.github.haifengl:smile-core:2.5.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
