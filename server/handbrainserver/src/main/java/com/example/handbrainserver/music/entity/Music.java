02378bb3ed3d96d6ad3cb58ba7d825103b5102b7
noir-01 <smu08054@gmail.com>
2024-11-07 01:27:23 +0900

    Feat: 비트 시간 분석
    엔티티, DTO 수정
    파일 업로드 시 python 파일 실행: librosa lib 필요

--- server/handbrainserver/src/main/java/com/example/handbrainserver/music/entity/Music.java
+++ server/handbrainserver/src/main/java/com/example/handbrainserver/music/entity/Music.java
@@ -1,15 +1,20 @@
 package com.example.handbrainserver.music.entity;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalTime;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Music {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +23,17 @@ public class Music {
     private String artist;
     private LocalTime duration;
     private String filePath;
+    @Lob
+    private String beatList;
+
+    // Convert JSON back to List<Float> after reading from the database
+    public List<Float> getBeatTime() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(this.beatList, List.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- server/handbrainserver/src/main/java/com/example/handbrainserver/music/service/MusicService.java
+++ server/handbrainserver/src/main/java/com/example/handbrainserver/music/service/MusicService.java
@@ -3,6 +3,7 @@ package com.example.handbrainserver.music.service;
 import com.example.handbrainserver.music.dto.MusicDto;
 import com.example.handbrainserver.music.entity.Music;
 import com.example.handbrainserver.music.repository.MusicRepository;
+import com.example.handbrainserver.music.util.BeatExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class MusicService {
         this.musicRepository = musicRepository;
     }
     public List<MusicDto> getAllMusic() {
-        List<Music> musicList = musicRepository.findAll();
+        List<MusicDto> musicList = musicRepository.findAllMusicWithoutFilePathAndBeatList();
         return musicRepository.findAll().stream() // Music 리스트를 스트림으로 변환
                 .map(MusicDto::from) // 각 Music 객체를 MusicDto로 변환
                 .collect(Collectors.toList());
@@ -29,7 +30,13 @@ public class MusicService {
         return music.map(Music::getFilePath).orElse(null);
     }
 
-    public void saveMusic(MusicDto musicDto){
-        musicRepository.save(musicDto.to());
+    public Long saveMusic(MusicDto musicDto){
+        //beat를 추가해서 repo에 저장.
+        String beatList = BeatExtractor.extractBeat(musicDto.getFilePath());
+        MusicDto.MusicWithBeatListDto musicWithBeatListDto = new MusicDto.MusicWithBeatListDto(
+                musicDto.getId(), musicDto.getTitle(), musicDto.getArtist(), musicDto.getDuration(), musicDto.getFilePath(), beatList
+        );
+        Music music = musicRepository.save(musicWithBeatListDto.to());
+        return music.getId();
     }
 }
