30dfc370bfcf0125dd12b1925547e29a7a934b6a
FYpingping <jbh3059@gmail.com>
2024-11-13 06:33:56 +0900

    error

--- MyApplication/app/src/main/java/com/example/myapplication/multiUi/MultiActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/multiUi/MultiActivity.kt
@@ -197,7 +197,7 @@ class MultiActivity : AppCompatActivity(), MainRecyclerViewAdapter.Listener, Mai
         runOnUiThread {
             views.apply {
                 val isVideoCall = model.type == DataModelType.StartVideoCall
-                incomingCallTitleTv.text = "${model.sender}이(가) 게임을 신청했어요!"
+                incomingCallTitleTv.text = "${model.sender}이(가) 게임을 신청했어요"
                 incomingCallLayout.isVisible = true
                 acceptButton.setOnClickListener {
                     getCameraAndMicPermission {
