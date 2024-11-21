8eff40befec67726e836411dab74795ddf30f9c8
noir-01 <blanc@DESKTOP-23KS9M5>
2024-11-21 23:36:05 +0900

    Server/Feat: 리듬게임기록 조회

--- server/handbrainserver/src/main/java/com/example/handbrainserver/music/repository/RhythmGameHistoryRepository.java
+++ server/handbrainserver/src/main/java/com/example/handbrainserver/music/repository/RhythmGameHistoryRepository.java
@@ -3,6 +3,7 @@ package com.example.handbrainserver.music.repository;
 import com.example.handbrainserver.music.dto.HistoryDto;
 import com.example.handbrainserver.music.dto.PeriodAverageDataDto;
 import com.example.handbrainserver.music.entity.RhythmGameHistory;
+import com.example.handbrainserver.music.entity.User;
 import com.example.handbrainserver.music.util.Difficulty;
 import com.example.handbrainserver.music.util.GameType;
 import jakarta.persistence.Tuple;
@@ -12,33 +13,13 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface RhythmGameHistoryRepository extends JpaRepository<RhythmGameHistory, Long> {
-    @Query("SELECT DATE(r.date), AVG(r.score) " +
-            "FROM RhythmGameHistory r " +
-            "WHERE r.user.id = :userId AND r.difficulty = :difficulty " +
-            "AND r.date >= :startDate " +
-            "GROUP BY DATE(r.date) " +
-            "ORDER BY DATE(r.date)")
-    List<Tuple> findDailyAverages(Long userId, Difficulty difficulty, LocalDate startDate);
-//
-//    @Query("SELECT new com.example.handbrainserver.music.dto.PeriodAverageDataDto(MIN(DATE(r.date)), AVG(r.reactionTime)) " +
-//            "FROM RhythmGameHistory r " +
-//            "WHERE r.user.id = :userId AND r.gameType = :gameType " +
-//            "AND r.date >= :startDate " +
-//            "GROUP BY YEAR(r.date), WEEK(r.date) " +
-//            "ORDER BY YEAR(r.date), WEEK(r.date)")
-//    List<PeriodAverageDataDto> findWeeklyAverages(@Param("userId") Long userId,
-//                                                             @Param("gameType") GameType gameType,
-//                                                             @Param("startDate") LocalDate startDate);
-//
-//    @Query("SELECT new com.example.handbrainserver.music.dto.PeriodAverageDataDto(MIN(DATE(r.date)), AVG(r.reactionTime)) " +
-//            "FROM RhythmGameHistory r " +
-//            "WHERE r.user.id = :userId AND r.gameType = :gameType " +
-//            "AND r.date >= :startDate " +
-//            "GROUP BY FUNCTION('DATE_FORMAT', r.date, '%Y-%m') " +
-//            "ORDER BY FUNCTION('DATE_FORMAT', r.date, '%Y-%m')")
-//    List<PeriodAverageDataDto> findMonthlyAverages(@Param("userId") Long userId,
-//                                                              @Param("gameType") GameType gameType,
-//                                                              @Param("startDate") LocalDate startDate);
+    //사용자의 주간 기록이 존재하는지 확인
+    @Query("SELECT h FROM RhythmGameHistory h WHERE h.user = :user AND h.date BETWEEN :startOfWeek AND :endOfWeek")
+    Optional<RhythmGameHistory> findMyTopRecordWeekly(User user, LocalDate startOfWeek, LocalDate endOfWeek);
+
+    @Query("SELECT h FROM RhythmGameHistory h WHERE h.date BETWEEN :startOfWeek AND :endOfWeek ORDER BY h.score DESC")
+    List<RhythmGameHistory> findAllUserRecordWeekly(LocalDate startOfWeek, LocalDate endOfWeek);
 }
