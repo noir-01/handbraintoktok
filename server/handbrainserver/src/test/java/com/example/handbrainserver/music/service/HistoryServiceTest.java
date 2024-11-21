44dadc06903359e6c1bb300692b7ffed7baccd0e
noir-01 <smu08054@gmail.com>
2024-11-22 00:45:05 +0900

    Server/Test: UserDto 변경 반영

--- server/handbrainserver/src/test/java/com/example/handbrainserver/music/service/HistoryServiceTest.java
+++ server/handbrainserver/src/test/java/com/example/handbrainserver/music/service/HistoryServiceTest.java
@@ -2,6 +2,7 @@ package com.example.handbrainserver.music.service;
 
 import com.example.handbrainserver.music.dto.HistoryDto;
 import com.example.handbrainserver.music.dto.PeriodAverageDataDto;
+import com.example.handbrainserver.music.dto.UserDto;
 import com.example.handbrainserver.music.util.GameType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,21 +23,21 @@ public class HistoryServiceTest {
     @Test
     public void saveAndCheck(){
         HistoryDto.RandomGameHistoryDto randomGameHistoryDto = new HistoryDto.RandomGameHistoryDto(
-                8L,
+                new UserDto(8L,"123"),
                 GameType.COPY,
                 200,
                 LocalDate.now().minusDays(3)
         );
         historyService.saveRandomGameHistory(randomGameHistoryDto);
         randomGameHistoryDto = new HistoryDto.RandomGameHistoryDto(
-                8L,
+                new UserDto(8L,"123"),
                 GameType.COPY,
                 300,
                 LocalDate.now().minusDays(2)
         );
         historyService.saveRandomGameHistory(randomGameHistoryDto);
         randomGameHistoryDto = new HistoryDto.RandomGameHistoryDto(
-                8L,
+                new UserDto(8L,"123"),
                 GameType.COPY,
                 400,
                 LocalDate.now().minusDays(1)
