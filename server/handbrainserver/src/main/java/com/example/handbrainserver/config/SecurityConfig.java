e54055770651750f3299496c444fd53d67b9c321
noir-01 <smu08054@gmail.com>
2024-11-26 00:42:16 +0900

    Server/Refactor: config package 추가

--- server/handbrainserver/src/main/java/com/example/handbrainserver/config/SecurityConfig.java
+++ server/handbrainserver/src/main/java/com/example/handbrainserver/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.example.handbrainserver.music.util;
+package com.example.handbrainserver.config;
 
+import com.example.handbrainserver.music.util.TokenAuthenticationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
