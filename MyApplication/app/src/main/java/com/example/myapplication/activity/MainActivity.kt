f0966e2246ecfd9d976fdd7f440bbeaf7902d73f
noir-01 <smu08054@gmail.com>
2024-12-10 21:27:42 +0900

    App/Fix: 메인 화면에서 뒤로가기 누를 시 dialog 디자인 수정

--- MyApplication/app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -6,11 +6,19 @@ import android.content.pm.PackageManager
 import android.widget.Toast
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.OnBackPressedCallback
 import com.example.myapplication.activity.AccountActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
     private var selectedGameName: String? = null
@@ -63,19 +71,31 @@ class MainActivity : BaseActivity() {
     }
 
     private fun showExitConfirmationDialog() {
-        // AlertDialog 생성
-        AlertDialog.Builder(this)
-            .setTitle("앱 종료")
-            .setMessage("정말 종료하시겠습니까?")
-            .setPositiveButton("예") { _, _ ->
-                // 앱 종료
+        val dialogView = layoutInflater.inflate(R.layout.dialog_custom_account, null)
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        //취소 동작 X
+        dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(false)
+
+        val textView = dialogView.findViewById<TextView>(R.id.dialog_message)
+        textView.text="게임을\n종료할까요?"
+        val buttonNo = dialogView.findViewById<Button>(R.id.button_no)
+        val buttonYes = dialogView.findViewById<Button>(R.id.button_yes)
+        // 예 버튼 클릭 시 동작
+        buttonYes.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                dialog.dismiss()
                 finishAffinity()
             }
-            .setNegativeButton("아니오") { dialog, _ ->
-                // 팝업 닫기
-                dialog.dismiss()
-            }
-            .show()
+        }
+        //아니오 버튼
+        buttonNo.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 
     private fun allPermissionGranted(): Boolean {
